94ceeff87ed50ab88660523a40b8f3c90bd0194a
Roman Samolazov <samolazov.roman.alex@gmail.com>
2021-05-08 16:28:39 +0300

    new version with grid

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,7 @@
 const btns=document.querySelectorAll(".img__arrow");
 const allSlide=document.querySelectorAll(".our-works__img");
+const btnsServ=document.querySelectorAll(".img-serv");
+
 let i=0;
 btns.forEach(btn=>{
     btn.addEventListener('click',()=>{
@@ -30,5 +32,25 @@ btns.forEach(btn=>{
         })
         allSlide[i].classList.add('img__activ');
         console.log(i);
+    })
+})
+btnsServ.forEach(btn=>{
+    btn.addEventListener('click',()=>{
+        if(btn.id=="btn-zoom"){
+            document.querySelector('#btn-zoom').classList.remove('img__activ');
+            document.querySelector('#btn-close').classList.add('img__activ');
+            allSlide.forEach(slide=>{
+                slide.classList.add('img__zoom');
+            })
+            
+        }
+        if(btn.id=="btn-close"){
+            document.querySelector('#btn-close').classList.remove('img__activ');
+            document.querySelector('#btn-zoom').classList.add('img__activ');
+            allSlide.forEach(slide=>{
+                slide.classList.remove('img__zoom');
+            })
+        }
+
     })
 })
